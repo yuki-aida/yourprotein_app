18ff673fa8e7789b3c4c1409b564f917b8298445
yuki-aida <red.pink.purple.ruby@gmail.com>
2019-11-06 04:37:40 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    # /users/:idから値を取得して@userに代入
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(name: "", email: "", password: "", password_confirmation:
+    #                     "")と同義
+    if @user.save
+      # 保存に成功した時の処理
+      flash[:success] = "Welcome to the YourProtein App!"
+      # redirect_to user_url(@user)と同義
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+  
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    get signup_path
+    # 無効なユーザーを登録して、データベースのユーザーの数が変わっていないことをテスト
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: {name: "",email: "",
+                      password: "foo", password_confirmation: "bar" } }
+    end
+    # users/newページにレンダリングされているかテスト
+    assert_template 'users/new'
+    # <div id = "error_explanation"></div>というHTMLがあるかどうかテスト
+    assert_select 'div#error_explanation'
+    # <div class = "alert"></div>というHTMLがあるかどうかテスト
+    assert_select 'div.alert'
+    # 'form[action="/signup"]'というHTMLがあるかどうかテスト
+    assert_select 'form[action="/signup"]'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    # assert_differenceブロック内の処理を実行する直前のUser.countの値と実行した後の
+    # 値を比較
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                          email: "user@example.com",
+                          password: "password",
+                          password_confirmation: "password" } }
+    end
+    # POSTリクエストを送信した結果を見て、指定されたリダイレクト先に移動するメソッド
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+  
+end
