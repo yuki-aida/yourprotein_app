5e1772887d4d37fd5f7e8a3c2863ec7eead8312b
yuki-aida <red.pink.purple.ruby@gmail.com>
2019-12-18 12:19:40 +0900

    update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_020542) do
+ActiveRecord::Schema.define(version: 20191207020542) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
