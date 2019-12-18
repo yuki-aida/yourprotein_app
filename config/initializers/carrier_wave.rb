f0f14233073f9b2a174d49fa1becb84e36915cfe
yuki-aida <red.pink.purple.ruby@gmail.com>
2019-12-19 01:13:25 +0900

    update carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       provider: 'AWS',
       aws_access_key_id: ENV['ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'　# S3バケット作成時に指定したリージョン。左記は東京を指す
+      region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'sampleapp21' # 作成したS3バケット名
+    config.fog_directory  = 'sampleapp21'
   end
 end
