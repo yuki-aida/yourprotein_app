45f48a9ad2f918b7de8851f76caa33f2922e24fe
yuki-aida <red.pink.purple.ruby@gmail.com>
2019-12-19 01:00:04 +0900

    update carrier_wava

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,20 +1,11 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'sampleapp21'
-    config.asset_host = 'https://s3.amazonaws.com/sampleapp21' #url間違い
     config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => 'ap-northeast-1' ,
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      path_style: true
+      provider: 'AWS',
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'　# S3バケット作成時に指定したリージョン。左記は東京を指す
     }
+    config.fog_directory  = 'sampleapp21' # 作成したS3バケット名
   end
 end
