77bae266973827c8cc7597cc100d63fff85f1119
yuki-aida <red.pink.purple.ruby@gmail.com>
2019-12-19 09:33:03 +0900

    update carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'sampleapp21'
+    config.fog_directory  = 's3-users-21'
   end
 end
